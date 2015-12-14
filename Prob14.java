84ff6577536edd0aa283491d4bd57764f59b9951
fisheyz <tiago_m4t3us@hotmail.com>
2015-12-14 17:42:22 +0000

    starting dfs

--- Prob14.java
+++ Prob14.java
@@ -2,9 +2,19 @@ import java.util.*;
 import java.io.*;
 
 public class Prob14{
-  static int n, l;
+  static int n,l ;          // nos do grafo
+  static boolean adj[][];   //matriz de adjacencias
+  static boolean visited[]; // visitados?
+
+  static void dfs(int v){
+
+
+  }
+  
 
   public static void main(String Args){
+
+
     Scanner in = new Scanner(System.in);
     n = in.nextInt();
     l = in.nextInt();
