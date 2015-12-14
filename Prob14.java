c0b26870df567df23cb732a860eac1a8f08a587c
fisheyz <tiago_m4t3us@hotmail.com>
2015-12-14 22:52:06 +0000

    working

--- Prob14.java
+++ Prob14.java
@@ -2,22 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class Prob14{
-  static int n,edges, count;          // nos do grafo
+  static int n,edges;        // nos do grafo
+  static int count = 0;
   static boolean adj[][];   //matriz de adjacencias
   static boolean visited[]; // visitados?
 
 
 
-  static int conex(int k){
-    for (int i = 1; i < n; i++)
-      if (!visited[i])
-        count += count;
-        dfs(i)
+  static void conex(int n){
+    for (int i = 1; i=< n; i++)
+      if (!visited[i]){
+        count += 1;
+        dfs(i);
+      }
   }
+
 
   static void dfs(int v){
     visited[v] = true;
-    for( int i = 1; i < n; i++)
+    for( int i = 1; i <=n; i++)
       if(!visited[i] && adj[v][i] )
         dfs(i);
 
@@ -36,11 +39,11 @@ public class Prob14{
       int a = in.nextInt();
       int b = in.nextInt();
       adj[a][b] = adj[b][a]  = true;
-      visited[i] = false;
+
 
     }
 
-    conex(1);
+    conex(n);
     System.out.print(count);
 
 
