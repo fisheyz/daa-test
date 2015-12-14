6f013636e7800c8cc5a5925ab1496dbcdf32dd31
fisheyz <tiago_m4t3us@hotmail.com>
2015-12-14 23:10:26 +0000

    new one

---
+++ Prob17.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Prob17{
+  static int n;
+  static String s[];
+
+  public static void main(String args[]){
+    Scanner in = new Scanner(System.in);
+    n = in.nextInt();
+    s = new String[n];
+    for (int i=0; i<n; i++)
+      s[i] = in.next();
+
+
+
+
+
+  }
+}
