847d3bfaffbf961406b615a46025703f91bad5c3
fisheyz <tiago_m4t3us@hotmail.com>
2015-12-14 17:16:00 +0000

    main done

--- Prob14.java
+++ Prob14.java
@@ -1,2 +1,15 @@
 import java.util.*;
 import java.io.*;
+
+public class Prob14{
+  static int n, l;
+
+  public static void main(String Args){
+    Scanner in = new Scanner(System.in);
+    n = in.nextInt();
+    l = in.nextInt();
+
+    for (int i=0; i < l; i++)
+
+  }
+}
